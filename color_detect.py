40d14bfcbe0323b5ec1e26b84aeb05333b0cccf8
Teppei Kanayana <1994teppei0615kanayama@gmail.com>
2017-07-01 21:26:24 +0900

    表紙画像に近い色を検出するmask画像を生成

--- color_detect.py
+++ color_detect.py
@@ -4,7 +4,8 @@ import cv2
 
 #探したい本の表紙画像を読み込み
 book_img = cv2.imread('./images/0.jpg', 1)
-target_img = cv2.imread('./images/target.jpg', 1)
+target_img = cv2.imread('./images/target2.jpg', 1)
+target_img_hsv = cv2.cvtColor(target_img, cv2.COLOR_BGR2HSV)
 
 height = len(book_img)
 width = len(book_img[0])
@@ -20,13 +21,19 @@ average_color = np.zeros((3, )).astype(np.uint8)
 for channel in range(channels):
     average_color[channel] = np.sum(book_img_leftside[:, :, channel]) / (height * int(width / 10))
 
-#色の平均値の表示
-average_color_image = np.ones((100, 100, 3)).astype(np.uint8)
-for channel in range(channels):
-    average_color_image[:, :, channel] *= average_color[channel]
+alpha = 5
+mask_img1 = np.ones(target_img_hsv[:, :, 0].shape) * (average_color[0] - alpha) < target_img_hsv[:, :, 0]
+mask_img2 = target_img_hsv[:, :, 0] < np.ones(target_img_hsv[:, :, 0].shape) * (average_color[0] + alpha)
+mask_img = (mask_img1 * mask_img2).astype(np.uint8) * 255
+print(mask_img1.shape, mask_img2.shape, mask_img.shape, book_img.shape, target_img_hsv.shape)
 
-cv2.namedWindow("average_color_image", cv2.WINDOW_NORMAL)
-cv2.imshow("average_color_image", average_color_image)
+#画像の表示
+resized_origin = cv2.resize(target_img, (int(mask_img.shape[0]/3), int(mask_img.shape[1]/3)))
+resized_dst = cv2.resize(mask_img, (int(mask_img.shape[0]/3), int(mask_img.shape[1]/3)))
+cv2.namedWindow("original_image", cv2.WINDOW_NORMAL)
+cv2.namedWindow("mask_image", cv2.WINDOW_NORMAL)
+cv2.imshow("original_image", resized_origin)
+cv2.imshow("mask_image", resized_dst)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
