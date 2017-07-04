efe20cb072ec783bc00536e0c50276f1b3c30902
Teppei Kanayana <1994teppei0615kanayama@gmail.com>
2017-07-04 17:38:03 +0900

    bounding box

--- image_processing.py
+++ image_processing.py
@@ -6,8 +6,10 @@ from line_detect import line_detect, line_detect2
 from segmentation import segmentation
 
 def main():
+    target = './images/target5.jpg'
+
     book_img = cv2.imread('./images/0.jpg', 1)
-    target_img = cv2.imread('./images/target.jpg', 1)
+    target_img = cv2.imread(target, 1)
 
     mask_img, masked_target_img = color_detect(book_img, target_img)
     #masked_target_img = line_detect2(book_img, target_img, masked_target_img)
@@ -42,9 +44,13 @@ def main():
     print(mask.shape)
     masked_segmented_img = segmented_img * mask
 
+    height = len(target_img)
+    width = len(target_img[0])
+    buf = 50
+    target_img = cv2.rectangle(target_img, (coord_list[index2][0], buf), (coord_list[index1][0], height - buf), (0, 0, 255), 20)
 
     #画像の表示
-    resized_origin = cv2.resize(masked_target_img, (int(mask_img.shape[0]/3), int(mask_img.shape[1]/3)))
+    resized_origin = cv2.resize(target_img, (int(mask_img.shape[0]/3), int(mask_img.shape[1]/3)))
     resized_dst = cv2.resize(mask_img, (int(mask_img.shape[0]/3), int(mask_img.shape[1]/3)))
     resized_dst2 = cv2.resize(masked_segmented_img, (int(masked_segmented_img.shape[0]/3), int(masked_segmented_img.shape[1]/3)))
     cv2.namedWindow("original_image", cv2.WINDOW_NORMAL)
@@ -55,6 +61,7 @@ def main():
     cv2.imshow("segmented_image", resized_dst2)
 
     cv2.waitKey(0)
+    cv2.imwrite("target5.jpg", resized_origin)
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
