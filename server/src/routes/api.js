2497a5be727239f414d82fa75527d2a190bd91c9
Kikurage <y.kikura@gmail.com>
2017-07-05 05:21:20 +0900

    feature: upload image and run detection

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -3,8 +3,16 @@ const router = express.Router();
 const debug = require('debug')('express:router:api');
 const multer = require('multer');
 const client = require('cheerio-httpcli');
+const rp = require('request-promise');
+const fs = require('fs');
+const util = require('util');
+const path = require('path');
+const exec = require('child_process').exec;
 
-router.post('/search', (req, res, next) => {
+const UPLOAD_FILE_ROOT = path.join(__dirname, '../../uploaded/');
+const ENGINE_CWD = path.join(__dirname, '../../../engine/');
+
+router.post('/search', async (req, res, next) => {
     let query = req.body.q;
     if (!query) {
         res.status(400);
@@ -13,48 +21,68 @@ router.post('/search', (req, res, next) => {
 
     debug('query', query);
 
-    client.fetch(`http://www.amazon.co.jp/s/?url=search-alias%3Dstripbooks&field-keywords=${encodeURIComponent(query)}`, (err, $, proxyRes, body) => {
-        let data = $('li[id^="result_"]')
-            .toArray()
-            .map((item, i) => {
-                let $item = $(item);
-                return {
-                    title: $item.find('h2').text(),
-                    image: $item.find('img').attr('src')
-                }
-            });
+    let amazonUrl = `http://www.amazon.co.jp/s/?url=search-alias%3Dstripbooks&field-keywords=${encodeURIComponent(query)}`;
+    let $ = await util.promisify(client.fetch)(amazonUrl);
 
-        debug(data);
+    let data = $('li[id^="result_"]')
+        .toArray()
+        .map((item, i) => {
+            let $item = $(item);
+            return {
+                title: $item.find('h2').text(),
+                image: $item.find('img').attr('src')
+            }
+        });
 
-        res.send(JSON.stringify(data));
-    });
+    debug(data);
+
+    res.send(JSON.stringify(data));
 });
 
-// router.post('/find', multer({dest: 'uploaded/'}).single('image'), (req, res, next) => {
-router.post('/find', multer({dest: 'uploaded/'}).none(), (req, res, next) => {
-    let imageUrl = req.body.imageUrl;
-    if (!imageUrl) {
+router.post('/find', multer({dest: 'uploaded/'}).single('targetImage'), async (req, res, next) => {
+    let timestamp = Date.now();
+
+    let bookImageUrl = req.body.bookImageUrl;
+    if (!bookImageUrl) {
         res.status(400);
         throw Error('imageUrl is null.');
     }
 
-    debug('imageUrl', imageUrl);
+    let bookImagePath = path.join(UPLOAD_FILE_ROOT, timestamp + path.extname(bookImageUrl));
+    debug('bookImageUrl', bookImageUrl);
+    debug('bookImagePath', bookImagePath);
 
-    // client.fetch(`http://www.amazon.co.jp/s/?url=search-alias%3Dstripbooks&field-keywords=${encodeURIComponent(query)}`, (err, $, proxyRes, body) => {
-    //     let data = $('li[id^="result_"]')
-    //         .toArray()
-    //         .map((item, i) => {
-    //             let $item = $(item);
-    //             return {
-    //                 title: $item.find('h2').text(),
-    //                 image: $item.find('img').attr('src')
-    //             }
-    //         });
-    //
-    //     console.log(data);
+    let targetImagePath = path.join(UPLOAD_FILE_ROOT, timestamp + '-target.jpg');
+    debug('targetImagePath', targetImagePath);
 
-    res.send('');
-    // });
+    let resultImagePath = path.join(UPLOAD_FILE_ROOT, timestamp + '-result.jpg');
+    debug('resultImagePath', resultImagePath);
+
+    try {
+        let res = await rp.get({
+            url: bookImageUrl,
+            encoding: null
+        });
+        await util.promisify(fs.writeFile)(bookImagePath, Buffer.from(res, 'utf8'));
+        await util.promisify(fs.rename)(path.join(process.cwd(), req.file.path), targetImagePath);
+    } catch (e) {
+        res.status(503);
+        throw e;
+    }
+
+    let command = `python image_processing.py ${bookImagePath} ${targetImagePath} ${resultImagePath}`;
+
+    try {
+        let result = await util.promisify(exec)(command, {
+            cwd: ENGINE_CWD
+        });
+
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.sendFile(resultImagePath);
+    } catch (e) {
+        res.status(503);
+        throw e;
+    }
 });
 
 module.exports = router;
--- web/src/scripts/index/containers/app/app.tsx
+++ web/src/scripts/index/containers/app/app.tsx
@@ -1,6 +1,8 @@
+import * as classNames from "classnames";
 import AppBar from "material-ui/AppBar";
 import { Card, CardActions, CardText } from "material-ui/Card";
 import FlatButton from "material-ui/FlatButton";
+import RaisedButton from "material-ui/RaisedButton";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import SearchIcon from "material-ui/svg-icons/action/search";
 import TextField from "material-ui/TextField";
@@ -11,36 +13,85 @@ interface Props extends React.ClassAttributes<HTMLDivElement> {
 }
 
 interface State {
-    json: ({ title: string, image: string })[]
+    json: ({ title: string, image: string })[],
+    activeView: ViewID,
+    bookImageUrl: string | null,
+    targetImage: File | null
+    resultImage: Blob | null
 }
 
+enum ViewID {
+    SearchView = 0,
+    PhotoView
+}
 class App extends React.Component<Props, State> {
     constructor() {
         super();
         this.state = {
-            json: []
+            json: [],
+            activeView: ViewID.SearchView,
+            bookImageUrl: null,
+            targetImage: null,
+            resultImage: null
         };
     }
 
     onItemClick(item: { title: string, image: string }) {
-        this.findBook(item.image)
+        this.setState({
+            bookImageUrl: item.image
+        });
+        this.activatePhotoView();
     }
 
     onSearchButtonTapped() {
         this.searchImages();
     }
 
-    async findBook(imageUrl: string) {
-        let data = new FormData();
-        data.append('imageUrl', imageUrl);
+    async onUploadPhotoButtonClick() {
+        let img = await new Promise<File>((resolve, reject) => {
+            let $input = document.createElement('input');
+            $input.type = 'file';
+            $input.multiple = false;
+            $input.accept = 'image/*';
+            $input.style.visibility = 'hidden';
+            $input.style.position = 'absolute';
+            document.body.appendChild($input);
 
-        let res = await fetch('/api/find', {
-            method: 'POST',
-            body: data
+            $input.onchange = async () => {
+                document.body.removeChild($input);
+                resolve($input.files![0]);
+            };
+
+            $input.click();
         });
 
-        let json = await res.json();
-        console.log(json);
+        this.setState({
+            targetImage: img
+        });
+        this.findBook();
+    }
+
+    activatePhotoView() {
+        this.setState({
+            activeView: ViewID.PhotoView
+        });
+    }
+
+    async findBook() {
+        let targetImage = this.state.targetImage!;
+        let data = new FormData();
+
+        data.append('bookImageUrl', this.state.bookImageUrl!);
+        data.append('targetImage', targetImage);
+
+        let resultImage = await (await fetch('/api/find', {
+            method: 'POST',
+            body: data
+        })).blob();
+
+        this.setState({
+            resultImage: resultImage
+        });
     }
 
     async searchImages() {
@@ -49,8 +100,11 @@ class App extends React.Component<Props, State> {
         if (query == '') return;
 
         this.setState({
-            json: []
+            json: [],
+            targetImage: null,
+            resultImage: null
         });
+
         let res = await fetch('/api/search', {
             method: 'POST',
             body: JSON.stringify({'q': query}),
@@ -77,28 +131,50 @@ class App extends React.Component<Props, State> {
                     display: 'none',
                 }} title="Book Finder"/>
                 <main className="App-Main">
-                    <Card>
-                        <CardText>
-                            <TextField ref="query"
-                                       style={{
-                                           width: '100%',
-                                           textAlign: 'center'
-                                       }}
-                                       floatingLabelText="書籍名"
-                                       floatingLabelFixed={true}
-                                       hintText="検索したい書籍名の一部を入力"/>
-                        </CardText>
-                        <CardActions>
-                            <FlatButton style={{width: '100%'}} label="検索" icon={<SearchIcon />}
-                                        onClick={() => this.onSearchButtonTapped()}/>
-                        </CardActions>
-                    </Card>
-                    <ul style={{
-                        flex: '1 1',
-                        overflow: 'auto',
-                    }}>
-                        {items}
-                    </ul>
+                    <section className="App-SearchView">
+                        <Card>
+                            <CardText>
+                                <TextField ref="query"
+                                           style={{
+                                               width: '100%',
+                                               textAlign: 'center'
+                                           }}
+                                           floatingLabelText="書籍名"
+                                           floatingLabelFixed={true}
+                                           hintText="検索したい書籍名の一部を入力"/>
+                            </CardText>
+                            <CardActions>
+                                <FlatButton style={{width: '100%'}} label="検索" icon={<SearchIcon />}
+                                            onClick={() => this.onSearchButtonTapped()}/>
+                            </CardActions>
+                        </Card>
+                        <ul style={{
+                            flex: '1 1',
+                            overflow: 'auto',
+                        }}>
+                            {items}
+                        </ul>
+                    </section>
+                    <section className={classNames('App-PhotoView', {
+                        'App-PhotoView-Show': this.state.activeView == ViewID.PhotoView
+                    })}>
+                        {this.state.resultImage ?
+                            <img style={{maxWidth: '100%', margin: '16px'}}
+                                 src={URL.createObjectURL(this.state.resultImage)}/> :
+                            this.state.targetImage ?
+                                <img style={{maxWidth: '100%', margin: '16px'}}
+                                     src={URL.createObjectURL(this.state.targetImage)}/> :
+                                null}
+                        <span>
+                            {this.state.resultImage ?
+                                '多分この辺' :
+                                this.state.targetImage ?
+                                    '本を検索中...' :
+                                    null}
+                        </span>
+                        <RaisedButton primary label="画像をアップロード"
+                                      onClick={() => this.onUploadPhotoButtonClick()}/>
+                    </section>
                 </main>
             </div>
         </MuiThemeProvider>);
